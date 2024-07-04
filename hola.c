d1b801e94ac9c071c852d3a3368a19b23f9fd83f
Δlain <ing.alainvargas@gmail.com>
2024-07-04 23:50:36 +0300

    Add basic comments

--- hola.c
+++ hola.c
@@ -2,11 +2,13 @@
 #include<linux/init.h>
 #include<linux/kernel.h>
 
-static int hello_init(void){
+// Function executed at the time of loading the driver and other when driver gets exited
+static int hello_init(void){   
    printk(KERN_ALERT "¡Hola desde el kernel! Inicia ejecución de controlador que no hace nada.\n");
    return 0;
 }
 
+// Function executed at the time of exiting the driver
 static void hello_exit(void){
    printk(KERN_INFO "¡Adiós desde el kernel! Termina ejecución de controlador.\n");
 }
@@ -14,5 +16,6 @@ static void hello_exit(void){
 module_init(hello_init);
 module_exit(hello_exit);
 
+// Module information
 MODULE_AUTHOR("Alain");
 MODULE_DESCRIPTION("Alain's first driver.");
