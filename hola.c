13413fd9669a7f79c77888b1e0f2b226fbd0ec61
Δlain <ing.alainvargas@gmail.com>
2024-07-07 13:07:29 +0300

    Add and improve comments

--- hola.c
+++ hola.c
@@ -1,8 +1,12 @@
-#include<linux/module.h>
-#include<linux/init.h>
-#include<linux/kernel.h>
+/* 
+ * hola.c - Basic kernel module.
+ */ 
 
-// Function executed at the time of loading the driver and other when driver gets exited
+#include<linux/module.h>    /* Needed by all modules */
+#include<linux/init.h>      /* Needed for the macros */
+#include<linux/kernel.h>    /* Needed for printk() */
+
+// Function executed at the time of loading the driver
 static int hello_init(void){   
    printk(KERN_ALERT "¡Hola desde el kernel! Inicia ejecución de controlador que no hace nada.\n");
    return 0;
@@ -13,6 +17,7 @@ static void hello_exit(void){
    printk(KERN_INFO "¡Adiós desde el kernel! Termina ejecución de controlador.\n");
 }
 
+// Methods are passed to the init and exit apis
 module_init(hello_init);
 module_exit(hello_exit);
 
